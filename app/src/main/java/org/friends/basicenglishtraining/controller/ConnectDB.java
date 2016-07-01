45b7ebdfd0e6179e40140e6a018b237ba2b5a6f6
javakarans <javakarans@gmail.com>
2016-07-02 01:06:49 +0430

    mirza

---
+++ app/src/main/java/org/friends/basicenglishtraining/controller/ConnectDB.java
@@ -0,0 +1,9 @@
+package org.friends.basicenglishtraining.controller;
+
+/**
+ * Created by Mohammad on 7/2/2016.
+ */
+public class ConnectDB {
+
+
+}
---
+++ app/src/main/java/org/friends/basicenglishtraining/controller/FetchingData.java
@@ -0,0 +1,8 @@
+package org.friends.basicenglishtraining.controller;
+
+/**
+ * Created by Mohammad on 7/2/2016.
+ */
+public class FetchingData {
+
+}
