5a2669822dc8b2001d21d137514a2e3205ed239b
javakarans <javakarans@gmail.com>
2016-07-01 19:58:38 +0430

    mirza

---
+++ app/src/main/java/org/friends/basicenglishtraining/controller/Contoller.java
@@ -0,0 +1,7 @@
+package org.friends.basicenglishtraining.controller;
+
+/**
+ * Created by Mohammad on 7/1/2016.
+ */
+public class Contoller {
+}
---
+++ app/src/main/java/org/friends/basicenglishtraining/model/Model.java
@@ -0,0 +1,7 @@
+package org.friends.basicenglishtraining.model;
+
+/**
+ * Created by Mohammad on 7/1/2016.
+ */
+public class Model {
+}
