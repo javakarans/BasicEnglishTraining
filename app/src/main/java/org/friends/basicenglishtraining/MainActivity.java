41a52305bb25e8568334a249a03c15ca397d968c
javakarans <javakarans@gmail.com>
2016-07-01 16:16:53 +0430

    mirza

--- app/src/main/java/org/friends/basicenglishtraining/MainActivity.java
+++ app/src/main/java/org/friends/basicenglishtraining/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    System.out.prntln("mirza asheghe cerine");
+    System.out.println("sina sexy");
     }
 }
